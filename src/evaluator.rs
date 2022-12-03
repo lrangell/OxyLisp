1be13377830d95e05e651e1613bf05eadcd3f118
Lucas Rangel <lucas@rangel.dev>
2022-12-03 16:16:42 -0300

    Delete evaluator

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -4,31 +4,7 @@ use anyhow::*;
 use std::collections::HashMap;
 
 pub fn eval(exp: Expression, env: &Env) -> Result<Primitive> {
-    let (toCall, forms) = exp;
-    let nf = forms
-        .iter()
-        .map(|f| match f {
-            Form::Primitive(p) => Ok(p.clone()),
-            Form::Symbol(s) => eval_object(&env.vars.get(s).expect("444")),
-            Form::Expression(e) => eval(e.clone(), env),
-        })
-        .collect::<Result<Vec<Primitive>>>()
-        .expect("444");
-
-    let cc = env.vars.get(&toCall).expect("ddd");
-
-    match cc {
-        Objects::BuiltinFn(f) => f(nf),
-        Objects::Primitive(p) => Ok(p.clone()),
-    }
-}
-
-fn eval_object(obj: &Objects) -> Result<Primitive> {
-    let res = match obj {
-        Objects::BuiltinFn(f) => None,
-        Objects::Primitive(p) => Some(p.clone()),
-    };
-    Ok(res.ok_or("444").expect("444"))
+    unimplemented!()
 }
 
 // TODO: implement into for types
