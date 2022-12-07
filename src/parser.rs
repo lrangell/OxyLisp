df5d8ebbcda9eec60370df94b1d2515ecc706807
Lucas Rangel <lucas@rangel.dev>
2022-12-07 20:18:13 -0300

    Add parser

---
+++ src/parser.rs
@@ -0,0 +1,27 @@
+use super::lexer;
+use crate::types::codeChunk::CodeChunk;
+use crate::types::*;
+use anyhow::*;
+use std::collections::HashMap;
+use std::vec::Vec;
+
+pub fn ast(tokens: Vec<Tokens>) -> Option<Form> {
+    debug!("AST Tokens:\n{:?}", tokens);
+    let (toCall, args) = tokens.split_first()?;
+    if let Tokens::Primitive(Primitive::String(s)) = toCall {
+        let code = CodeChunk {
+            tokens: args.to_vec(),
+        };
+        let args: Vec<Form> = code.into_iter().collect();
+        // let expression = Form::Expression((s.to_string(), args));
+        debug!(
+            "ast: Expression:\nSymbol: {}\n Args: {:?} ",
+            s.to_string(),
+            args
+        );
+        let expression = Form::Expression((s.to_string(), args));
+        return Some(expression);
+    }
+    debug!("Unable to parse tokens: {:?}", tokens);
+    None
+}
