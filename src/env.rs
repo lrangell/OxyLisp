d7b1e259a8e47b5eb103f170aa4918bd1f06fd95
Lucas Rangel <lucas@rangel.dev>
2022-12-07 20:17:35 -0300

    Add + and * to env

--- src/env.rs
+++ src/env.rs
@@ -1,10 +1,8 @@
+use crate::prelude::*;
+use anyhow::{Context, Ok, Result};
 use std::collections::hash_map;
 
-use anyhow::{Context, Ok, Result};
-
-use crate::types::*;
-
-pub fn aritimetic() -> (String, Objects) {
+pub fn aritimetic() -> [(String, Objects); 2] {
     let add: BuiltinFn = |nums: Vec<Primitive>| -> Result<Primitive> {
         nums.iter()
             .filter_map(|s| match s {
@@ -13,14 +11,26 @@ pub fn aritimetic() -> (String, Objects) {
             })
             .reduce(|acc, curr| acc + curr)
             .map(Primitive::Integer)
-            // .ok_or(Err("aaa"))
             .context("All arguments must be integers")
     };
-    return ("+".to_string(), Objects::BuiltinFn(add));
+    let mult: BuiltinFn = |nums: Vec<Primitive>| -> Result<Primitive> {
+        nums.iter()
+            .filter_map(|s| match s {
+                Primitive::Integer(i) => Some(*i),
+                _ => None,
+            })
+            .reduce(|acc, curr| acc * curr)
+            .map(Primitive::Integer)
+            .context("All arguments must be integers")
+    };
+    return [
+        ("+".to_string(), Objects::BuiltinFn(add)),
+        ("*".to_string(), Objects::BuiltinFn(mult)),
+    ];
 }
 
 pub fn init_env() -> Env {
     return Env {
-        vars: hash_map::HashMap::from([aritimetic()]),
+        vars: hash_map::HashMap::from(aritimetic()),
     };
 }
