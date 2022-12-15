6e4e86da0f09237bf0979af48b46208fe9dc2fb2
Lucas Rangel <lucas@rangel.dev>
2022-12-15 00:16:26 -0300

    Add repl

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,5 @@ mod tests;
 mod types;
 
 fn main() {
-    env_logger::init();
-    let defcode = "(def xx (+ 50 (* 25 2)) )";
-    // let addcode = "(+ 1 (* 10 2) 2 100)";
-    let addcode = "(+ xx (* 10 2) )";
-
-    let mut basic_env = env::init_env();
-    let __r = evaluator::eval_from_str(defcode, &mut basic_env);
-    let r = evaluator::eval_from_str(addcode, &mut basic_env);
-    println!("{:?}", r);
+    repl::init()
 }
---
+++ src/repl/mod.rs
@@ -0,0 +1,37 @@
+use crate::env::init_env;
+use crate::evaluator::eval_from_str;
+use reedline::{DefaultPrompt, FileBackedHistory, Prompt, Reedline, Signal};
+
+pub fn init() {
+    let mut env = init_env();
+    let history = Box::new(
+        FileBackedHistory::with_file(5, "/tmp/oxy_history.txt".into())
+            .expect("Error configuring history with file"),
+    );
+
+    let mut line_editor = Reedline::create().with_history(history);
+    let prompt = DefaultPrompt::new(
+        reedline::DefaultPromptSegment::Basic("λ ".to_string()),
+        reedline::DefaultPromptSegment::Empty,
+    );
+
+    loop {
+        let sig = line_editor.read_line(&prompt);
+        match sig {
+            Ok(Signal::Success(buffer)) => {
+                let res = eval_from_str(&buffer, &mut env);
+                match res {
+                    Ok(primitive) => println!("{}", primitive.first().unwrap()),
+                    Err(e) => println!("Error: {}", e.to_string()),
+                }
+            }
+            Ok(Signal::CtrlD) | Ok(Signal::CtrlC) => {
+                println!("\nAborted!");
+                break;
+            }
+            x => {
+                println!("Event: {:?}", x);
+            }
+        }
+    }
+}
