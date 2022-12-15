0c9f4676b57a18fd19616f3ac2cab686f75bff37
Lucas Rangel <lucas@rangel.dev>
2022-12-14 23:48:06 -0300

    Error handling

--- src/parser.rs
+++ src/parser.rs
@@ -7,21 +7,21 @@ pub fn parse(tokens: &[Tokens]) -> Result<Vec<Form>> {
         let r: Vec<Form> = [].to_vec();
         return Ok(r);
     }
-    let (head, tail) = tokens.split_first().unwrap();
+    let (head, tail) = tokens
+        .split_first()
+        .ok_or(anyhow!("\nEmpty token vector\n"))?;
 
     debug!("head: {:?} tail: {:?}", head, tail);
 
     match &head {
         Tokens::Bounds(TokenBounds::LeftParen) => {
-            if let (Tokens::Symbol(sym), right_tokens) = tail.split_first().unwrap() {
+            if let (Tokens::Symbol(sym), right_tokens) =
+                tail.split_first().ok_or("Unexpected empty parens")?
+            {
                 let (args_tokens, rest) =
                     split_at_bound(right_tokens, Tokens::Bounds(TokenBounds::RightParen))?;
                 let args = parse(args_tokens)?;
                 let remaining_forms = parse(rest)?;
-                debug!(
-                    "parseing () expr: args: {:?} rest: {:?} ",
-                    args, remaining_forms
-                );
                 let mut call_expr = vec![Form::CallExpression((sym.clone(), args))];
                 call_expr.extend(remaining_forms);
                 return Ok(call_expr);
@@ -30,7 +30,7 @@ pub fn parse(tokens: &[Tokens]) -> Result<Vec<Form>> {
             }
         }
         Tokens::Bounds(TokenBounds::LeftBracket) => {
-            let (inner, rest) = split_at_bound(tokens, Tokens::Bounds(TokenBounds::RightBracket))?;
+            let (inner, rest) = split_at_bound(tail, Tokens::Bounds(TokenBounds::RightBracket))?;
             let inner_forms = parse(inner)?;
             let rest_forms = parse(rest)?;
             let mut list_form = vec![Form::List(Box::new(inner_forms))];
@@ -58,7 +58,11 @@ fn split_at_bound(tokens: &[Tokens], closing_bound: Tokens) -> Result<(&[Tokens]
     let n = tokens
         .iter()
         .rposition(|t| *t == closing_bound)
-        .ok_or(anyhow!(""))?;
+        .ok_or(anyhow!(
+            "\n\nUnable to find closing delimiter: {}\nIn the tokens: {:?}\n\n",
+            closing_bound,
+            tokens
+        ))?;
     let (inner, rest) = tokens.split_at(n);
     Ok((inner, rest))
 }
