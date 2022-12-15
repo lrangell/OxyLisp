f5e1b508a584eadc62464d990db5ae886006917c
Lucas Rangel <lucas@rangel.dev>
2022-12-14 23:04:52 -0300

    Use proper variable names

--- src/parser.rs
+++ src/parser.rs
@@ -13,12 +13,18 @@ pub fn parse(tokens: &[Tokens]) -> Result<Vec<Form>> {
 
     match &head {
         Tokens::Bounds(TokenBounds::LeftParen) => {
-            if let (Tokens::Symbol(sym), rest) = tail.split_first().unwrap() {
-                let (args, rr) = split_at_bound(rest, Tokens::Bounds(TokenBounds::RightParen))?;
-                let mut ca = [Form::CallExpression((sym.clone(), parse(args).unwrap()))].to_vec();
-                let rrr = parse(rr).unwrap();
-                ca.extend(rrr);
-                return Ok(ca);
+            if let (Tokens::Symbol(sym), right_tokens) = tail.split_first().unwrap() {
+                let (args_tokens, rest) =
+                    split_at_bound(right_tokens, Tokens::Bounds(TokenBounds::RightParen))?;
+                let args = parse(args_tokens)?;
+                let remaining_forms = parse(rest)?;
+                debug!(
+                    "parseing () expr: args: {:?} rest: {:?} ",
+                    args, remaining_forms
+                );
+                let mut call_expr = vec![Form::CallExpression((sym.clone(), args))];
+                call_expr.extend(remaining_forms);
+                return Ok(call_expr);
             } else {
                 return Err(anyhow!("First element of a form must be a symbol"));
             }
@@ -27,9 +33,9 @@ pub fn parse(tokens: &[Tokens]) -> Result<Vec<Form>> {
             let (inner, rest) = split_at_bound(tokens, Tokens::Bounds(TokenBounds::RightBracket))?;
             let inner_forms = parse(inner)?;
             let rest_forms = parse(rest)?;
-            let mut a = vec![Form::List(Box::new(inner_forms))];
-            a.extend(rest_forms);
-            Ok(a)
+            let mut list_form = vec![Form::List(Box::new(inner_forms))];
+            list_form.extend(rest_forms);
+            Ok(list_form)
         }
         Tokens::Literal(l) => {
             let uu = parse(tail)?;
@@ -40,10 +46,10 @@ pub fn parse(tokens: &[Tokens]) -> Result<Vec<Form>> {
         Tokens::Bounds(TokenBounds::RightParen) => parse(tail),
         Tokens::Bounds(TokenBounds::RightBracket) => parse(tail),
         Tokens::Symbol(s) => {
-            let uu = parse(tail)?;
-            let mut r = [Form::Symbol(s.to_owned())].to_vec();
-            r.extend(uu);
-            Ok(r)
+            let remaining_forms = parse(tail)?;
+            let mut symbol_form = [Form::Symbol(s.to_owned())].to_vec();
+            symbol_form.extend(remaining_forms);
+            Ok(symbol_form)
         } // &s => Ok(vec![s.into()]),
     }
 }
