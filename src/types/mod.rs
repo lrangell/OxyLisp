d82845a0c5d0fd8a75a2f4ec0aea53e6c45174e2
Lucas Rangel <lucas@rangel.dev>
2022-12-21 15:02:14 -0300

    Recursive scope lookup for symbols

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,16 +1,16 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
-pub mod Errors;
 pub mod display;
 use anyhow::{anyhow, Result};
+use log::debug;
 use std::{collections::HashMap, fmt};
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 pub enum TokenBounds {
     LeftParen,
     RightParen,
-    LeftBracket, 
-    RightBracket
+    LeftBracket,
+    RightBracket,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -18,12 +18,6 @@ pub enum Tokens {
     Bounds(TokenBounds),
     Literal(Literal),
     Symbol(String),
-}
-
-#[derive(Debug, Clone)]
-struct Lambda {
-    args: HashMap<String, Form>,
-    body: Vec<Form>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -41,7 +35,7 @@ pub enum Form {
     Literal(Literal),
     CallExpression(CallExpression),
     Symbol(String),
-    List(Box<Vec<Form>>)
+    List(Box<Vec<Form>>),
 }
 
 pub type CallExpression = (String, Vec<Form>);
@@ -51,24 +45,72 @@ trait Eval {
 }
 
 #[derive(Clone)]
+pub struct Lambda {
+    pub name: Option<String>,
+    pub args: Vec<String>,
+    pub body: Vec<Form>,
+    pub env: Env,
+}
+
+#[derive(Clone)]
 pub enum RuntimeObject {
     Primitive(Literal),
     Function(BuiltinFn),
+    RuntimeFunction(Lambda),
 }
 
+#[derive(Clone)]
+pub enum EnvType {
+    RootEnv,
+    LambdaEnv(Box<Env>),
+}
+
+#[derive(Clone)]
 pub struct Env {
     pub vars: HashMap<String, RuntimeObject>,
-    // def: fn(Self, sym: Form, &RuntimeObject) -> Result<()>,
+    pub parent: EnvType,
 }
 
 impl Env {
     pub fn def(&mut self, symbol: &Form, value: &RuntimeObject) -> Result<RuntimeObject> {
-        let Form::Symbol(sym) = symbol else { 
+        let Form::Symbol(sym) = symbol else {
             return Err(anyhow!("First argument of def must be a symbol"))
         };
         debug!("def sym: {} val: {}", sym, value);
         self.vars.insert(sym.clone(), value.clone());
         Ok(RuntimeObject::Primitive(Literal::Bool(true)))
+    }
+    pub fn defn(
+        self: &mut Box<Self>,
+        symbol: &Form,
+        arguments: Vec<String>,
+        forms: Vec<Form>,
+    ) -> Result<RuntimeObject> {
+        let Form::Symbol(sym) = symbol else {
+            return Err(anyhow!("First argument of defn must be a symbol"))
+        };
+        let function = Lambda::new(
+            Some(symbol.to_string()),
+            arguments.iter().map(|a| a.to_string()).collect(),
+            forms,
+            &self,
+        );
+        self.vars.insert(
+            sym.to_string(),
+            RuntimeObject::RuntimeFunction(function).clone(),
+        );
+        Ok(RuntimeObject::Primitive(Literal::Bool(true)))
+    }
+
+    pub fn lookup(&self, symbol: &str) -> Option<RuntimeObject> {
+        debug!("Env: {self}");
+        if let Some(value) = self.vars.get(symbol) {
+            return Some(value.clone());
+        }
+        match &self.parent {
+            EnvType::RootEnv => None,
+            EnvType::LambdaEnv(parent) => parent.lookup(symbol),
+        }
     }
 }
 
@@ -93,8 +135,6 @@ impl From<Form> for Literal {
         }
     }
 }
-
-
 
 impl From<i32> for Literal {
     fn from(p: i32) -> Self {
